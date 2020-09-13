5b161fc65846d580a24b1293651523f26daaad63
Catherine Gloria <catherinegloria@gmail.com>
2020-09-13 18:36:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,53 @@
-# Add  code here!
+def prime?(num) #4
+  if num >= 2
+    array = (2...(num - 1)).to_a #[2,3]
+    new_array = array.map do |i| #[false,]
+      if num % i == 0
+        "not_prime"
+      else
+        "prime"
+      end
+    end
+  else num < 2 || num.negative?()
+    false
+  end
+
+  if new_array.include?("not_prime")
+    false
+  else num < 2 || num.negative?()
+    true
+  end
+end
+
+#  if new_array.include?("not_prime")
+#    false
+#  elsif num.negative?() || num < 2
+#    false
+#  else
+#    true
+#  end
+#end
+
+#how to make false for 0 and 1
+
+def def_prime?(num)
+  if num >= 2
+    array = (2...(num - 1)).to_a
+    new_array = array.map do |i|
+      if num % i == 0
+        "not_prime"
+      else
+        "prime"
+      end
+    end
+    new_array
+  end
+
+  if num.negative?() || num < 2
+    false
+  elsif new_array.include?("not_prime")
+    false
+  else
+    true
+  end
+end
